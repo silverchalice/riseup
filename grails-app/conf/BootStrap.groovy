52ad7acc98ac96cfa21e3905cc8b181835404ac0
Benjamin Klein <ben@silver-chalice.com>
2015-03-31 18:57:53 -0500

    create TicketType instances in BootStrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,14 @@
+import riseup.TicketType
+
 class BootStrap {
 
     def init = { servletContext ->
+
+        if(TicketType.count() == 0){
+            def t1 = new TicketType(description: 'Single', price: 399.00).save(failOnError: true)
+            def t2 = new TicketType(description: 'Double', price: 299.00).save(failOnError: true)
+        }
+
     }
     def destroy = {
     }
