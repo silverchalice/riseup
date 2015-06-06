304b33cf43c1aebd012a2c6197679895c439526c
Benjamin Klein <ben@silver-chalice.com>
2015-06-06 11:33:32 -0500

    don't create new TicketType instances (for now)

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -4,10 +4,10 @@ class BootStrap {
 
     def init = { servletContext ->
 
-        if(TicketType.count() == 0){
+        /*if(TicketType.count() == 0){
             def t1 = new TicketType(description: 'Single', price: 399.00).save(failOnError: true)
             def t2 = new TicketType(description: 'Double', price: 299.00).save(failOnError: true)
-        }
+        }*/
 
     }
     def destroy = {
