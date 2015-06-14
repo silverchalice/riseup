47d356c672ffd507efd6218577e36a2fd43b2f82
Benjamin Klein <ben@silver-chalice.com>
2015-06-14 17:37:59 -0500

    add 'zip', make 'address2' nullable, add toString() (Buyer.groovy)

--- grails-app/domain/riseup/Buyer.groovy
+++ grails-app/domain/riseup/Buyer.groovy
@@ -8,8 +8,14 @@ class Buyer {
     String address2
     String city
     String state
+    String zip
     String phone
 
     static constraints = {
+        address2 nullable: true
+    }
+
+    String toString(){
+        name
     }
 }
