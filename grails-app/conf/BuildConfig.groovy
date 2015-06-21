0eeb872d53ebe6b74d76c6d971eb46bda873f401
David Klein <davidklein@Davids-MacBook-Pro-2.local>
2015-06-21 17:20:23 -0500

    Adding hibernate plugin back in.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -63,13 +63,13 @@ grails.project.dependency.resolution = {
         // plugins for the compile step
         compile ":scaffolding:2.1.2"
         compile ":mail:1.0.7"
-        //compile ':cache:1.1.7'
+        compile ':cache:1.1.7'
         compile ":asset-pipeline:1.9.6"
         compile ":jquery-ui:1.10.4"
         
         // plugins needed at runtime but not for compilation
-        //runtime ":hibernate4:4.3.5.5" // or ":hibernate:3.6.10.17"
-        //runtime ":database-migration:1.4.0"
+        runtime ":hibernate4:4.3.5.5" // or ":hibernate:3.6.10.17"
+        runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
 
         // Uncomment these to enable additional asset-pipeline capabilities
